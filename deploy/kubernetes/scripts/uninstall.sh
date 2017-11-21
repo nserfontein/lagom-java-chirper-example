e91e4a49eaec5c04d816cda685d574a69fe825f0
Nico Serfontein <nserfontein@gmail.com>
2017-11-21 11:06:49 +0200

    Install script

--- deploy/kubernetes/scripts/uninstall.sh
+++ deploy/kubernetes/scripts/uninstall.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 kubectl delete pod activityservice-0
-kubectl delete pod cassandra-0
 kubectl delete pod chirpservice-0
 kubectl delete pod friendservice-0
 kubectl delete pod web-0
@@ -14,7 +13,6 @@ kubectl delete service friendservice-akka-remoting
 kubectl delete service web
 
 kubectl delete statefulset activityservice
-kubectl delete statefulset cassandra
 kubectl delete statefulset chirpservice
 kubectl delete statefulset friendservice
 kubectl delete statefulset web
